7033b833890c70c224b21cffe1c31a1debada102
Jonathan <johnjonathan948@gmail.com>
2021-09-18 06:14:01 -0700

    some init msg

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let navBtn = document.querySelector('.nav-btn').addEventListener('click', function btn() {
+  let x = document.querySelector('.kk').style.display = 'block';
+let rr = document.querySelector('.kk').style.height = '100%';
+
+})
+
+let closeBtn = document.getElementById('jj').addEventListener('click', function closeBtn() {
+  let x = document.querySelector('.kk').style.display = 'none';    
+})
